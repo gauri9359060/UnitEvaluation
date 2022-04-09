89727949264a807c4f0bbe4671cc5a465eaa9a72
Gauri Patil <72353440+gauri9359060@users.noreply.github.com>
2022-04-09 15:06:16 +0530

    Initialize project using Create React App

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavbarWrapper = styled.nav`
+    display:flex;
+    flex-direction:row;
+    justify-content:flex-end;
+    background-color: teal;
+    color:white;
+    height:100px;
+`
+
+const A = styled(Link)`
+text-decoration:none;
+color:white;
+margin:30px;
+font-size:30px;
+cursor:pointer;
+`
+const Navbar = () => {
+  return (
+    <NavbarWrapper>
+        <A to="/">Home</A>
+        <A to="/products">Products</A>
+    </NavbarWrapper>
+  )
+}
+
+export default Navbar
